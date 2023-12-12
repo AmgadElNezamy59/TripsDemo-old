a0032bf6971820f175ea9f9d069095018eb77147
Amgad Elnezamy <amgad.bue@gmail.com>
2023-12-12 06:50:06 +0200

    Initial Commit

---
+++ Trips Demo/Trips_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trips_DemoApp.swift
+//  Trips Demo
+//
+//  Created by Amgad Salah on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Trips_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
